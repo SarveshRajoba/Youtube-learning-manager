daaba88f3001f9341db7d5c997c509548dea92a1
SarveshRajoba <132903409+SarveshRajoba@users.noreply.github.com>
2025-12-07 00:37:11 +0530

    Add keepalive route for health checks

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -36,7 +36,9 @@ Rails.application.routes.draw do
   patch '/profile', to: 'users/profile#update'
   patch '/profile/password', to: 'users/profile#update_password'
   
-  
+  # config/routes.rb
+get "/keepalive", to: proc { [200, {}, ["OK"]] }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
